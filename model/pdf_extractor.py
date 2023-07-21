6be03d76c2fac031e0b600b6b5e85d3bf7670229
yrribeiro <ynk9a@outlook.com>
2023-07-21 17:07:57 -0300

    initial ipynb and pdf extractor files

--- model/pdf_extractor.py
+++ model/pdf_extractor.py
@@ -1,14 +1,16 @@
-from PyPDF2 import PdfReader
-  
-# creating a pdf reader object
-reader = PdfReader('pdfs/oferta-2023-1.pdf')
-  
-# printing number of pages in pdf file
-print(len(reader.pages))
-  
-# getting a specific page from the pdf file
-page = reader.pages[0]
-  
-# extracting text from page
-text = page.extract_text()
-print(text)
+import pdfquery
+import pandas as pd
+import re
+
+FILE = 'pdfs/oferta-2023-1.pdf'
+pdf = pdfquery.PDFQuery(FILE)
+pdf.load()
+# pdf.tree.write('pdfXML.xml', pretty_print = True)
+# employer_name = pdf.pq('LTTextLineHorizontal:overlaps_bbox("98.4, 512.147, 143.182, 519.034")').text()
+# print(employer_name)
+text_elements = pdf.pq('LTRect')
+
+# Extract the text from the elements
+text = [t.text for t in text_elements]
+
+print(text_elements)
