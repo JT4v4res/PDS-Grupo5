b55ee9edca82bbd971fa2244c4e86f10b1841f50
thalyssa <tam@ic.ufal.br>
2023-08-25 12:03:44 -0300

    Implementa o componente de material de estudo

--- magicasper/src/pages/MateriasDetalhes/index.js
+++ magicasper/src/pages/MateriasDetalhes/index.js
@@ -1,14 +1,17 @@
 import "@fontsource/reem-kufi";
 import './index.css';
-
 import {MdOutlineViewHeadline} from 'react-icons/md';
 
-function MateriaDetalhes (materia, nivelEsforco, codigo, descrição, professores){
+function MateriaDetalhes (materia, nivelEsforco, codigo, descrição, professores, matExpositivo, literatura, questoes){
   materia = "Estrutura de dados"
   nivelEsforco = ['Baixo', 'Médio', 'Alto']
   codigo = "BP336CB"
   descrição = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.  Sed aliquam sem sodales Nullam tempus pretium est, nec gravida felis tempus quis. Sed aliquam sem sodales tempor eleifend asid cinoyt bec Hissa.asid cinoyt bec Hissa."
   professores = ['Ranilson', 'Willy Tiengo', 'Rodrigo Paes', 'Roberta Lopez']
+  matExpositivo = ['Material 1', "Material 2"]
+  literatura = ['Material 1', 'Material 2']
+  questoes = ['Questão 1', 'Questão 2']
+
 return (
 <div id='Conteudo-Disciplinas'>
     <div className='materia'>
@@ -37,6 +40,30 @@ return (
           </ul>
         </div>
       </div>
+    </div>
+
+    <div id="material-estudo">
+      <div className="material-label">Material de Estudo</div>
+      <div className="main-box">
+              <ul className="lista-mat-expositivo">
+                <div className="lista-label">Material Expositivo</div>
+                <br></br>
+                  <li key={matExpositivo[0]}>{matExpositivo[0]}</li>
+                  <li key={matExpositivo[1]}>{matExpositivo[1]}</li>
+              </ul>
+              <ul className="lista-literatura">
+                <div className="lista-label">Material na Literatura</div>
+                <br></br>
+                  <li key={literatura[0]}>{literatura[0]}</li>
+                  <li key={literatura[1]}>{literatura[1]}</li>
+              </ul>
+              <ul className="lista-questoes">
+                <div className="lista-label">Questões importantes</div>
+                <br></br>
+                  <li key={questoes[0]}>{questoes[0]}</li>
+                  <li key={questoes[1]}>{questoes[1]}</li>
+              </ul>
+          </div>
     </div>
 </div>
 )
