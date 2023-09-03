4d10d8e156b22bc4c91ef1aceaccc30c6922adc6
palomallacerda <psls@ic.ufal.br>
2023-09-03 20:34:52 -0300

    indicadores+avaliacaoEstreda v1

--- magicasper/src/Componentes/Avaliacao-Estrela/index.js
+++ magicasper/src/Componentes/Avaliacao-Estrela/index.js
@@ -3,19 +3,13 @@ import './index.css'
 
 function AvaliacaoEstrela(avaliacao){
   let tam_avaliacao;
-  for (let index = 0; index < avaliacao; index++) {
-    tam_avaliacao.apend('1');
-  }
+  
   console.log(tam_avaliacao)
   return(
     <>
     <div id='avaliacao'>
       <ul>
-      {
-          tam_avaliacao.map(tam_avaliacao => (
               <li className='icone-avaliacao'> </li>
-          ))
-      }
       </ul>
     </div>
     </>
--- magicasper/src/pages/MateriaIndicadores/index.js
+++ magicasper/src/pages/MateriaIndicadores/index.js
@@ -1,18 +1,67 @@
 import './index.css';
 import SideBar from '../../Componentes/SideBar';
-
+import AvaliacaoEstrela from '../../Componentes/Avaliacao-Estrela';
 import SeletorCurso from "../../Componentes/Seletor-Curso";
 
-function MateriaIndicadores (materia){
+function MateriaIndicadores (materia, codigo, dadosIndicadores, nivelEsforco, informativos){
   materia = "Estrutura de dados"
-return (
+  codigo = "BP336CB"
+  nivelEsforco = ['Baixo', 'Médio', 'Alto']
+  dadosIndicadores = ['Cerca de 80% dos alunos são aprovados na 1ª tentativa', '88%  não recomendam pegar  essa matéria se não viu o básico de programação', '50% afirmam que o seu professor foi didático', '44% dos usuários já concluíram essa matéria', '90% dos alunos recomendam essa matéria no inicio do curso', '97% afirmam que essa matéria requer muito tempo de dedicação']
+  informativos = ['Mais de 70 alunos já avaliaram essa matéria', 'A última avaliação  foi feita em 10/10/2024']
+  return (
   <>
   <div className='page-content'>
-    <SideBar/>
-    <SeletorCurso texto={materia}/>
+    
+    <div className='title-content'>
+      <SideBar/>
+      <SeletorCurso texto={materia}/>
+      <AvaliacaoEstrela/>
+      <div className='esforco'>
+        <label key={nivelEsforco}>Nível de esforço: {nivelEsforco[2]}</label>
+      </div>
+      
+    </div>
+    <div className='codigo'>
+      <label>Código: {codigo}</label>
+    </div>
+    <div className='main-area'>
+      <div className='jornada-aluno'>
+
+        <div className='title'>
+          <div className="linha-esq"></div>
+          <div className="linha-acima"></div>
+          <label>Jornada do <span> aluno </span> </label>
+          <div className="linha-dir"></div>
+          <div className="linha-abaixo"></div>
+        </div>
+        <div className='cards'>
+            <ul className="dados-indicadores">
+                  {
+                      dadosIndicadores.map(dadosIndicadores => (
+                          <li>
+                              <a href='/'>{dadosIndicadores}</a>
+                          </li>
+                      ))
+                  }
+              </ul>
+        </div>
+      </div>
+
+      <div className='informativos-escuro'>
+        <ul className="dados-informativos">
+                {
+                    informativos.map(informativos => (
+                        <li>
+                            <a href='/'>{informativos}</a>
+                        </li>
+                    ))
+                }
+          </ul>
+      </div>
+    </div>
   </div>
    
-    <h1>MateriaIndicadores</h1>
   </>
 )
 
