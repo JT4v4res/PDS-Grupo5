a4462659884252f9a556156844d53991ef3889da
yrribeiro <ynk9a@outlook.com>
2023-09-23 18:25:19 -0300

    login page done

--- magicasper/src/pages/Login/index.js
+++ magicasper/src/pages/Login/index.js
@@ -1,6 +1,7 @@
 import './index.css'
 import userLogo from '../../img/img-commenter.png';
 import React, { useState } from 'react';
+import { FaEye } from 'react-icons/fa';
 
 export default function Login(){
   const [username, setUsername] = useState('');
@@ -20,6 +21,21 @@ export default function Login(){
     console.log('Senha:', password);
   };
 
+  // const passwordInput = document.getElementById('password');
+  // const passwordToggle = document.querySelector('.password-toggle');
+
+  // passwordToggle.addEventListener('click', function () {
+  //   if (passwordInput.type === 'password') {
+  //     passwordInput.type = 'text';
+  //     passwordToggle.classList.remove('fa-eye');
+  //     passwordToggle.classList.add('fa-eye-slash');
+  //   } else {
+  //     passwordInput.type = 'password';
+  //     passwordToggle.classList.remove('fa-eye-slash');
+  //     passwordToggle.classList.add('fa-eye');
+  //   }
+  // });
+
   return(
     <>
       <div className='page-content'>
@@ -29,43 +45,51 @@ export default function Login(){
           </h1>
         </div>
 
+        <div className='main-container'>
+
         <div className='img-container'>
           <img src={userLogo}/>
         </div>
 
         <div className='login-container'>
           <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div id='usuario'>
-              <label htmlFor="username">Usuário</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={handleUsernameChange}
-                placeholder='Digite seu CPF ou e-mail'
-              />
-            </div>
-            <div id='senha'>
-              <label htmlFor="password">Senha</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={handlePasswordChange}
-                placeholder='Digite sua senha'
-              />
-            </div>
-            <div className='text'>
-              <span>Para alterar a senha clique aqui</span>
-              <span>Cadastre-se</span>
-            </div>
-            <div className='button-box'>
-              <button type="submit">Entrar</button>
-            </div>
-          </form>
+          <div className='form-box'>
+            <form onSubmit={handleSubmit}>
+              <div id='usuario'>
+                <label htmlFor="username">Usuário</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={handleUsernameChange}
+                  placeholder='Digite seu CPF ou e-mail'
+                  style={{ color: 'black' }}
+                />
+              </div>
+              <div id='senha'>
+                <label htmlFor="password">Senha</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  placeholder='Digite sua senha'
+                  style={{ color: 'black' }}
+                />
+              {/* <FaEye class="password-toggle"/> */}
+              </div>
+              <div className='button-box'>
+                <button type="submit">Entrar</button>
+              </div>
+            </form>
+          </div>
 
+          <div className='help-text'>
+              <a href=''><span>Esqueci a senha</span></a>
+              <a href='/Cadastro'><span>Cadastre-se</span></a>
+          </div>
 
+        </div>
         </div>
       </div>
     </>
