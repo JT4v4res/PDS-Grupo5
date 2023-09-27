f93c608ba9242d5abcb99f936a9c44a47be0eded
palomallacerda <psls@ic.ufal.br>
2023-09-27 13:37:51 -0300

    add verificação de login MagiCasper

--- magicasper/src/Componentes/Navbar/index.js
+++ magicasper/src/Componentes/Navbar/index.js
@@ -1,20 +1,33 @@
 import './index.css';
-import { Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import "@fontsource/reem-kufi";
-import {MdOutlineSettings} from 'react-icons/md'
+
 
 function Navbar  () {
+  const UserLogged = true; 
 return <header>
         <nav id="navbar">
           <div className="container">
-            <h1 className="logo">
-              <Link to="/MagicasperInicial">Magi Casper</Link>
-            </h1>
-            <ul>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  UserLogged ? (
+                    <h1 className="logo">
+                      <Link to="/Home">Magi Casper</Link>
+                    </h1>
+                  ) : (
+                    <h1 className="logo">
+                      <Link to="/MagicasperInicial">Magi Casper</Link>
+                    </h1>
+                  )
+                }
+              />
+            </Routes>
+                    <ul>
               <li><Link to="/Home">Home</Link></li>
               <li><Link to="/Perfil">Perfil</Link></li>
               <li><Link to="/Materias">Materias</Link></li>
-              {/* <li className='setting-icon'><MdOutlineSettings/></li> */}
             </ul>
           </div>
         </nav>
