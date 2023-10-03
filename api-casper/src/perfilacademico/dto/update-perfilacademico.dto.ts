192346d7f16c7acc5b60786b118164a5dcc94c81
thalyssa <tam@ic.ufal.br>
2023-10-03 02:59:15 -0300

    Transferência dos arquivos de perfil acadêmico para a sua própria branch

--- api-casper/src/perfilacademico/dto/update-perfilacademico.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePerfilacademicoDto } from './create-perfilacademico.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
-export class UpdatePerfilacademicoDto extends PartialType(CreatePerfilacademicoDto) {
-
-    @IsNotEmpty()
-    @IsString()
-    matricula: string;
-}
