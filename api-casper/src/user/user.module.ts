f90d6724a0073a37d439655a6c83fd8dec981024
JT4v4res <jvst@ic.ufal.br>
2023-10-05 15:48:29 -0300

    Implementação de login no back-end

--- api-casper/src/user/user.module.ts
+++ api-casper/src/user/user.module.ts
@@ -4,9 +4,18 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { PerfilacademicoEntity } from 'src/perfilacademico/entities/perfilacademico.entity';
+import { JwtModule } from '@nestjs/jwt';
+import * as process from 'process';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, PerfilacademicoEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, PerfilacademicoEntity]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '21600s' },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [TypeOrmModule, UserService],
