25a8d72ed7103b1f276f53e6ff6fea3121a44d3c
palomallacerda <psls@ic.ufal.br>
2023-10-18 11:42:40 -0300

    Atualização de dados no cadastro

--- magicasper/src/Componentes/SegForm/index.js
+++ magicasper/src/Componentes/SegForm/index.js
@@ -1,87 +1,105 @@
-import './index.css'
-import userLogo from '../../img/img-commenter.png';
-import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
-// import { useForm } from 'react-hook-form';
+  import './index.css'
+  import userLogo from '../../img/img-commenter.png';
+  import { useNavigate } from 'react-router-dom';
+  import React, { useState } from 'react';
+  // import { useForm } from 'react-hook-form';
+  import { useLocation } from 'react-router-dom';
+  
+  export default function SegundoCadastro(props){
+      const [matricula, setMatricula] = useState('');
+      const [password, setPassword] = useState('');
+      const [isButtonEnabled, setIsButtonEnabled] = useState(false);
 
-export default function SegundoCadastro(props){
-    const [matricula, setMatricula] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    // const register = useForm();
+      const navigate = useNavigate();
 
-    const handleMatriculaChange = (event) => {
-        setMatricula(event.target.value);
-    };
+      const location = useLocation();
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
+      // Acesso às informações
+      const { name, mail, curso } = location.state;
+      // console.log("Nome1", name);
+      // console.log("email1", mail);
+      // console.log("curso1", curso);
 
-    const handleSubmit = (event) => {
-        navigate('/Home')
-        event.preventDefault();
-    };
+      const handleMatriculaChange = (event) => {
+          setMatricula(event.target.value);
+      };
 
-  return(
-    <>
-      <div className='page-content-1'>
-        <div className='magi-casper'>
-          <h1>
-            Magi Casper
-          </h1>
-        </div>
+      const handlePasswordChange = (event) => {
+          setPassword(event.target.value);
+          validateForm()
+      };
 
-        <div className='main-container'>
+      const handleSubmit = (event) => {
+          navigate('/Login')
+          event.preventDefault();
+      };
 
-        <div className='img-container'>
-          <img src={userLogo}/>
-        </div>
+      const validateForm = () => {
+        setIsButtonEnabled(password);
+      };
 
-        <div className='cadastro-container'>
-          <h2>Cadastro</h2>
-          <div className='form-box'>
-            <form onSubmit={handleSubmit}>
+      const inputErrorStyle = {
+        color: 'red', // Define a cor do texto como vermelho
+        border: '1px solid red', // Adicione uma borda vermelha
+      };
+    
 
-            <div id='usuario'>
-                <label htmlFor="username">Matrícula</label>
-                <input
-                    type="text"
-                    id="username"
-                    value={matricula}
-                    onChange={handleMatriculaChange}
-                    placeholder='Digite sua matrícula'
-                    style={{ color: 'black' }}
-                />
-            </div>
-            <div id='senha'>
-                <label htmlFor="password">Senha</label>
-                <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    placeholder='Digite sua senha'
-                    style={{ color: 'black' }}
-                    // ref={register({
-                    //   required: 'Password is required.',
-                    //   minLength: {
-                    //     value: 6,
-                    //     message: 'Password should have at-least 6 characters.'
-                    //   }
-                    // })}                               TODO
-                />
-            </div>
-
-            <div className='button-box'>
-                <button type="submit">Inscrever-se</button>
-            </div>
-            </form>
+    return(
+      <>
+        <div className='page-content-1'>
+          <div className='magi-casper'>
+            <h1>
+              Magi Casper
+            </h1>
           </div>
 
+          <div className='main-container'>
+
+          <div className='img-container'>
+            <img alt='#' src={userLogo}/>
+          </div>
+
+          <div className='cadastro-container'>
+            <h2>Cadastro</h2>
+            <div className='form-box'>
+              <form onSubmit={handleSubmit}>
+
+              <div id='usuario'>
+                  <label htmlFor="username">Matrícula</label>
+                  <input
+                      type="text"
+                      id="username"
+                      value={matricula}
+                      onChange={handleMatriculaChange}
+                      placeholder='Digite sua matrícula'
+                      style={{ color: 'black' }}
+                  />
+              </div>
+              <div id='senha'>
+                  <label htmlFor="password">Senha</label>
+                  <input
+                      type="password"
+                      id="password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                      placeholder='Digite sua senha'
+                      style={password ? {color:'black'} : inputErrorStyle}
+                  />
+              </div>
+
+              <div className='button-box'>
+                  <button type="submit"
+                  disabled={!isButtonEnabled}
+                  >
+                    {isButtonEnabled ? 'Cadastre-se' : 'Desabilitado'}
+                </button>
+              </div>
+              </form>
+            </div>
+
+          </div>
+          </div>
         </div>
-        </div>
-      </div>
-    </>
-  )
-}
+      </>
+    )
+  }
