6c49224705e61ccbf248cca0636e096ad4f05348
palomallacerda <psls@ic.ufal.br>
2023-10-22 16:33:36 -0300

    corrigindo bug nas áreas relevantes+na localstorage

--- magicasper/src/context/context.js
+++ magicasper/src/context/context.js
@@ -3,51 +3,47 @@ import { createContext, useState } from "react";
 
 export const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
-  // Obtenha o token do localStorage ao carregar a página
-  const storedToken = localStorage.getItem('token');
-  const [token, setToken] = useState(storedToken || '');
-  const [userId, setUserId] = useState('');
-  const [signed, setSigned] = useState(!!storedToken); // Verifique se o token está presente no localStorage
+export const AuthProvider = ({children}) => {
+    // const storedToken = localStorage.getItem('token');
+    const [token, setToken] = useState('');
+    const [userId, setUserId] = useState('');
+    const [signed, setSigned] = useState(false);
 
-  async function signIn(email, password) {
-    try {
-      const response = await api.post("/user/login", {
-        email: email,
-        senha: password,
-      });
-
-      if (response !== undefined && response !== null) {
-        // Armazene o token no localStorage
-        localStorage.setItem('token', response.data.access_token);
-
-        setToken(response.data.access_token);
-        setUserId(response.data.id);
-        setSigned(true);
-      } else {
-        console.error('Usuário não encontrado!');
-      }
-    } catch (err) {
-      if (err.response) {
-        console.error(`Ocorreu um erro: ${err.response.data}`);
-      } else {
-        console.error('Ocorreu um erro desconhecido.');
-      }
+    async function signIn(email, password){
+        await api.post("/user/login", {
+            email: email,
+            senha: password,
+        })
+        .then((response) => {
+            console.log("response", response)
+            if (response !== undefined && response !== null) {
+                localStorage.setItem('token', response.data.access_token);
+                setToken(response.data.access_token);
+                setUserId(response.data.id);
+                setSigned(true);
+            } else {
+                console.error('Usuário não encontrado!');
+            }
+        })
+        .catch((err) => {
+            if (err.response) {
+                console.error(`Ocorreu um erro: ${err.response.data}`);
+            } else {
+                console.error('Ocorreu um erro desconhecido.');
+            }
+        });
     }
-  }
 
-  function logOut() {
-    // Remova o token do localStorage ao fazer logout
-    localStorage.removeItem('token');
+    function logOut() {
+      //Remova o token do localStorage ao fazer logout
+      localStorage.removeItem('token');
 
-    setToken('');
-    setUserId('');
-    setSigned(false);
-  }
+      setToken('');
+      setUserId('');
+      setSigned(false);
+    }
 
-  return (
-    <AuthContext.Provider value={{ signed, userId, authToken: token, signIn, logOut }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (<AuthContext.Provider value={{signed: signed, userId: userId, authToken: token, signIn, logOut}}>
+        {children}
+    </AuthContext.Provider>)
 }
